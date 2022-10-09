88092836812ba9a44dc59394657078cf249f8154
Ravineel Singhi <rajatsinghi89@gmail.com>
2022-10-09 19:52:04 +0530

    setup

--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@ echo "............................."
 
 # Install the required packages
 sudo apt-get install -y python3.8
-sudo apt-get install -y python3-pip
-sudo apt-get install -y python3-venv
+sudo apt-get install -y python3.8-pip
+sudo apt-get install -y python3.8-venv
 
 python3 -m venv env
 source env/bin/activate
