2303e7ec70c2f83c7dcf64f7532dea0114c4585c
Ravineel Singhi <rajatsinghi89@gmail.com>
2022-10-17 02:54:10 +0530

    models

---
+++ app.py
@@ -0,0 +1,33 @@
+import os
+from re import template
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Api, Resource
+from Application.config import LocalDevelopmentConfig
+from Application.database import db
+
+
+app=None
+api-None
+
+def create_app():
+  app=Flask(__name__,template_folder='templates',static_folder='assets')
+  if os.getenv('ENV',"development")=='Production':
+    raise Exception("Not available")
+
+  else:
+    print("Running in development mode")
+    app.config.from_object(LocalDevelopmentConfig)
+  
+  db.init_app(app)
+  api=Api(app)
+  app.app_context().push()
+  return app,api
+
+app,api = create_app()
+
+from Application.APIs import *
+
+
+if __name__ == "__main__":
+  app.run(debug=True, host='0.0.0.0', port=5000)
--- setup.sh
+++ setup.sh
@@ -3,12 +3,12 @@ echo "Please wait ................."
 echo "............................."
 
 # Install the required packages
-sudo apt-get install -y python3.8
-sudo apt-get install -y python3.8-pip
-sudo apt-get install -y python3.8-venv
+sudo apt-get install -y python3.10
+sudo apt-get install -y python3.10-pip
+sudo apt-get install -y python3.10-venv
 
 python3 -m venv env
-source env/bin/activate
+source ./env/bin/activate
 pip install -r requirements.txt
 deactivate
 
